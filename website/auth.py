9e34200c965fe1c4a10e54b6f23ebfd50254a80a
kedabudre <abdurehimk77@gmail.com>
2024-06-19 15:12:05 +0300

    update file

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
-from werkzoug import generate_password_hash, check_password_hash
+
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -48,7 +50,7 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords do not match', category='error')
         else:
-            new_user = User(email=email, first_name=name, password=generate_password_hash(password1), methods='sha256')
+            new_user = User(email=email, first_name=name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account successfully created!', category='success')
