23b11983ad7c146550e3a06f0a87aeaa582ec0c0
kedabudre <abdurehimk77@gmail.com>
2024-06-20 03:58:13 +0300

    update file

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 
 views = Blueprint('views', __name__)
@@ -21,3 +22,17 @@ def home():
             flash('Note saved succesful!', category='success')
 
     return render_template('home.html', user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            flash('delete successful!', category='success')
+
+    return jsonify({})
